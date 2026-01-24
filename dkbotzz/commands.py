d16ab45ad31b9d561125381a02d5cee6566ab540
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2026-01-24 12:43:01 +0530

    Start Cmd
    Added Start Cmd

---
+++ dkbotzz/commands.py
@@ -0,0 +1,13 @@
+from pyrogram import Client as DKBOTZ, filters
+from pyrogram.types import *
+from Config import *
+from dkbotz_text import *
+
+# /start cmd
+@DKBOTZ.on_message(filters.command("start"))
+async def dkbotz_handle_start(bot, message):
+    await message.reply_text(
+        START_MESSAGE,
+        reply_markup=start_buttons(),
+        disable_web_page_preview=True
+    )
