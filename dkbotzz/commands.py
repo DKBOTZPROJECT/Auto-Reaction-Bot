cc32af2a218f7b0475eb39890ab21f32aaf9d2b0
DKBOTZ <108566855+DKBOTZPROJECT@users.noreply.github.com>
2026-01-24 12:46:38 +0530

    A Little Thing

--- dkbotzz/commands.py
+++ dkbotzz/commands.py
@@ -2,10 +2,12 @@ from pyrogram import Client as DKBOTZ, filters
 from pyrogram.types import *
 from Config import *
 from dkbotz_text import *
+from dkbotzz.reaction import *
 
 # /start cmd
 @DKBOTZ.on_message(filters.command("start"))
 async def dkbotz_handle_start(bot, message):
+    await send_reaction(bot, message)
     await message.reply_text(
         START_MESSAGE,
         reply_markup=start_buttons(),
